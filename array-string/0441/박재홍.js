bd6741f85f187df83726f351e81d79fdeea51d2f
artisan <73726591+JaeHongDev@users.noreply.github.com>
2023-01-12 16:27:40 +0900

    solve(0441) : 새로운 방법 추가

--- array-string/0441/박재홍.js
+++ array-string/0441/박재홍.js
@@ -10,3 +10,18 @@ var arrangeCoins = function(n) {
     }
     return n === 0 ? i : i -1;
 };
+
+// 등차수열을 이용한 다른 방법 메모리 등수 순위 올라감 ( 49.33% -> 59.3% )
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var arrangeCoins1 = function(n) {
+    
+    for(let i = 1 ; i <= n ; i ++){ 
+        const result = (i ** 2 + i ) / 2;
+        if(result === n) return i;
+        if(result > n) return i-1;
+    }
+    return 0;
+};
