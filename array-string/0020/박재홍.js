b0c158b8514d86d7613f94d121eda09640841eac
artisan <73726591+JaeHongDev@users.noreply.github.com>
2023-01-13 15:38:02 +0900

    solve(0020) : 성능 및 메모리 사용량 개선

--- array-string/0020/박재홍.js
+++ array-string/0020/박재홍.js
@@ -25,3 +25,35 @@ var isValid = function(s) {
     }
     return stack.length === 0 
 };
+
+
+
+//Runtime: 55 ms, faster than 99.7% of JavaScript online submissions for Valid Parentheses.
+//Memory Usage: 41.4 MB, less than 99.48% of JavaScript online submissions for Valid Parentheses.
+
+//시간복잡도 o(n)
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid1 = function(s) {
+    
+    const stack = []; 
+
+    const compare = (target,original) => {
+        if(original === "(") return target === ")";
+        if(original === "[") return target === "]";
+        if(original === "{") return target === "}";
+        return false;
+    }
+    const maxLength = s.length;
+    for(let i = 0 ; i < maxLength;i++ ){
+        if(stack.size !== 0 && compare(s[i], stack[stack.length-1])){
+            stack.pop();
+            continue; 
+        } 
+        stack.push(s[i]);
+    }
+    return stack.length === 0 
+};
+
